4b20a10ec574c96a338787184fe7b75d351464b8
Yana <anazigaluk@gmail.com>
2025-04-17 17:12:34 +0300

    solution bulls cows

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+/* eslint-disable no-console */
 'use strict';
 
-// Write your code here
+const { checkIsValidUserInput } = require('./modules/checkIsValidUserInput');
+const { generateRandomNumber } = require('./modules/generateRandomNumber');
+const { getBullsAndCows } = require('./modules/getBullsAndCows');
+
+const realNumber = generateRandomNumber();
+
+function playGame() {
+  const guessNumber = process.argv.slice(2)[0];
+
+  if (checkIsValidUserInput(guessNumber)) {
+    console.log(getBullsAndCows(+guessNumber, realNumber));
+  }
+}
+
+playGame();
